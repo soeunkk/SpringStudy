eac51234bbdb8430bcdd772c1bf98ce3b0f8ff4e
soeunkk <luckyber1@naver.com>
2022-02-20 12:35:04 +0900

    [feat] Member 도메인과 레포지토리 (#7)
    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 (김영한)
    Chapter_06 로그인 처리1 - 쿠키, 세션
    Part_04 회원가입

--- login/src/main/java/hello/login/domain/member/MemberRepository.java
+++ login/src/main/java/hello/login/domain/member/MemberRepository.java
@@ -1,2 +1,48 @@
-package hello.login.domain.member;public class MemberRepository {
+package hello.login.domain.member;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Slf4j
+@Repository
+public class MemberRepository { //원래는 인터페이스로 만든 후에 구현체를 만드는 것이 좋음 (유지보수성 향상)
+
+    private static Map<Long, Member> store = new HashMap<>();   //static 사용
+    private static long sequence = 0L;  //static 사용
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        log.info("save: member={}", member);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public Optional<Member> findByLoginId(String loginId) {
+//        List<Member> all = findAll();
+//        for (Member m : all) {
+//            if (m.getLoginId().equals(loginId)) {
+//                return Optional.of(m);
+//            }
+//        }
+//        return Optional.empty();
+
+        //lambda 와 stream 으로 간단하게 작성하기 (기본으로 할 줄 알아야 함!)
+        return findAll().stream()
+                .filter(m -> m.getLoginId().equals(loginId))
+                .findFirst();
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
